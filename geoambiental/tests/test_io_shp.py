ad12fb2d73a7e52f3de95deaf07370b8b2b0e085
Mario Villasante <mario.villasante@islas.org.mx>
2022-07-04 14:47:03 -0700

    🧪🛑 Set test multi2single

--- geoambiental/tests/test_io_shp.py
+++ geoambiental/tests/test_io_shp.py
@@ -1,6 +1,15 @@
-from geoambiental import import_coast_line
-
+import geopandas as gp
+from geoambiental import import_coast_line, multi2single
 
 def test_import_coast_line():
     file_path = "geoambiental/tests/data/linea_costa_isla_guadalupe.shp"
     import_coast_line(file_path)
+    
+def test_multi2single():    
+    path = "geoambiental/tests/data/linea_costa_isla_guadalupe.shp"
+    poligonos = gp.read_file(path)
+    projection = "epsg:4484"
+    poligonos.crs = {"init": projection}
+    poligonos_geograficas = poligonos.to_crs({"init": "epsg:4326"})
+    poligonos_geograficas = multi2single(poligonos_geograficas)
+
